b0898549463bb3f1ce620a6839650beccf96e4dc
Dom <https://github.com/cfreitas0>
2025-12-24 07:04:34 -0300

    server

--- scripts_python/srvidor_tcp.py
+++ scripts_python/srvidor_tcp.py
@@ -3,11 +3,18 @@ import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 try:
-  server.bind(("0.0.0.0", 9999))
+  server.bind(("0.0.0.0", 7878))
   server.listen(5)
-  print("listen...")
+  print("Conectado ...")
+  
+  client_socket, address = server.accept()
+  print("Conexão estabelecida:", address[0])
 
-  server.close()
+  while True:
+      data = client_socket.recv(1024).decode("utf-8")
+      if data == "senhasecreta\n":
+        client_socket.send(b"Acesso Permitido")
 
 except Exception as error:
-  print("Erro: =>", error)
+    print("Erro: =>", error)
+    server.close()
