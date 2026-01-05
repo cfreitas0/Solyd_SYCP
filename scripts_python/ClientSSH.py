025e8c88535ec73a298cb02dbfb89d67ff3e9ce4
Seu Nome <mariguecarlos@gmail.com>
2026-01-05 13:06:40 -0300

    ssh

--- scripts_python/ClientSSH.py
+++ scripts_python/ClientSSH.py
@@ -4,10 +4,20 @@ host = "127.0.0.1"
 user = "dom"
 passwd = "nvme"
 
-try:
-    client = paramiko.SSHClient()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(host, username=user, password=passwd)
-except Exception as e:
-    print('Erro =>', e) 
+
+client = paramiko.SSHClient()
+client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+client.connect(host, username=user, password=passwd)
+
+while True:
+    stdin, stduot, stderr = client.exec_command(input('Digite um Comando: '))
+    for line in stduot.readlines():
+        print(line.strip())
+
+    erro = stderr.readlines()
+    if erro:
+        print(erro)
+
+
+    
     
